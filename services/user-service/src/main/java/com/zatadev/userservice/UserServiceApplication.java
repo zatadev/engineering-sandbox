36b1cd79571f5ecd9f861292dd165dadce30a3f1
zakariya tahri <zakariya.tahri@zatadev.com>
2026-06-18 17:33:28 +0000

    feat: add @EnableSpringDataWebSupport VIA_DTO on user-service and order-service

--- services/user-service/src/main/java/com/zatadev/userservice/UserServiceApplication.java
+++ services/user-service/src/main/java/com/zatadev/userservice/UserServiceApplication.java
@@ -2,8 +2,10 @@ package com.zatadev.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
